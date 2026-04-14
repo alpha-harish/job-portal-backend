59d4bc93ba8ba685d6b464c51c0fb7396dcc224e
Harish S <harish03461@gmail.com>
2026-04-14 22:02:31 +0530

    Move backend into backend folder structure

--- server/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-const morgan = require('morgan');
-
-const logger = (req, res, next) => {
-  const format = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
-  return morgan(format)(req, res, next);
-};
-
-module.exports = logger;
